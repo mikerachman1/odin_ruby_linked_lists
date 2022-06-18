aad79134d8caa601336473a9ca3344bc758f14b7
Mike Rachman <mikerachman1@gmail.com>
2022-06-18 14:13:05 -0400

    All methods complete in LinkedList class

--- main.rb
+++ main.rb
@@ -21,6 +21,16 @@ class LinkedList
         @head = node
         @tail = @head
     end
+   
+    def find_by_value(value, like_find)
+        current = @head
+        @size.times do
+            return like_find ? current : true if current.value == value
+
+            current = current.next_node
+        end
+        like_find ? nil : false
+    end
 
     def append(value)
         node = Node.new(value)
@@ -49,13 +59,91 @@ class LinkedList
     def size
         puts "Total nodes = #{@size}"
     end
+
+    def head
+        puts "Head node: #{@head}"
+    end
+
+    def tail
+        puts "Tail node: #{@tail}"
+    end
+
+    def at(index)
+        current = @head
+        i = 0 
+        loop do
+            return current if index == i
+            current = current.next_node
+            throw 'index out of range' if current.nil?
+            i += 1
+        end
+    end
+
+    def pop
+        popped = @tail
+        @tail = at(@size - 2)
+        @tail.next_node = nil
+        @size -= 1
+        popped
+    end
+
+    def contains?(value)
+        find_by_value(value, false)
+    end
+
+    def find(value)
+        find_by_value(value, true)
+    end
+
+    def to_s
+        current = @head
+        string = ''
+        loop do 
+            break if current.nil?
+
+            string << "[#{current.value}] ->"
+            break if current.next_node.nil?
+
+            current = current.next_node
+        end
+        "#{string}nil"
+    end
+
+    def insert_at(value, index)
+        return prepend(value) if index.zero?
+        return append(value) if index == @size
+
+        node = Node.new(value)
+        prev_node = at(index - 1)
+        node.next_node = prev_node.next_node
+        prev_node.next_node = node
+        node
+    end
+
+    def remove_at(index)
+        return pop if index == @size - 1
+        return @head = @head.next_node if index.zero?
+
+        prev_node = at(index - 1)
+        prev_node.next_node = prev_node.next_node.next_node
+    end
+        
 end
 
         
 list = LinkedList.new
 
 list.append(10)
-list.size
+list.append(15)
+list.prepend(3)
+list.prepend(7)
+list.append(5)
+
+p list.to_s
+
+list.remove_at(2)
+list.insert_at(88, 1)
+p list.to_s
         
         
 
